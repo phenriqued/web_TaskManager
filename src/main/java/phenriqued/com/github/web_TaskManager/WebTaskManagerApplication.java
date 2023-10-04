e00cb82c4cf0db50b9b9335dc3efe09231991e4e
Pedro <pedrodias3737@gmail.com>
2023-10-04 14:49:58 -0300

    Piloto

---
+++ src/main/java/phenriqued/com/github/web_TaskManager/WebTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package phenriqued.com.github.web_TaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phenriqued/com/github/web_TaskManager/WebTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package phenriqued.com.github.web_TaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
